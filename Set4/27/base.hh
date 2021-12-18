a9cc09c0249fb5b74a32d311e48df0186a8a25fa
albert.campana <albert.campana@estudiantat.upc.edu>
2021-12-18 12:15:01 +0100

    enum

--- Set4/27/base.hh
+++ Set4/27/base.hh
@@ -6,19 +6,7 @@
 class Base
 {
     public:
-        enum class Msg
-        {
-            NONE = 0,
-            DEBUG   = 1 << 0,
-            INFO    = 1 << 1,
-            NOTICE  = 1 << 2,
-            WARNING = 1 << 3,
-            ERR     = 1 << 4,
-            CRIT    = 1 << 5,
-            ALERT   = 1 << 6,
-            EMERG   = 1 << 7,
-            ALL = (1 << 8) - 1,
-        };
+        enum class Msg;
 };
 
 inline constexpr size_t valueOf(Base::Msg msg)
